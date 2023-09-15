5b01513833f8caead05601d756fad26be380e610
Nauris Linde <naurislinde@gmail.com>
2023-09-15 18:50:33 +0000

    Improved Logger

--- src/core/logger.h
+++ src/core/logger.h
@@ -25,18 +25,15 @@ class Logger final : public Singleton<Logger> {
 
   // Creates SPDLOG logger with multiple sinks (console + file)
   void initLogger() {
-    m_ConsoleSink = CreateRef<spdlog::sinks::stdout_color_sink_mt>();
-    m_FileSink = CreateRef<spdlog::sinks::basic_file_sink_mt>(
+    auto console_sink = CreateRef<spdlog::sinks::stdout_color_sink_mt>();
+    auto file_sink = CreateRef<spdlog::sinks::basic_file_sink_mt>(
         parameters::log_path, false);
 
-    m_ConsoleSink->set_level(spdlog::level::trace);
-    m_FileSink->set_level(spdlog::level::trace);
+    console_sink->set_pattern("[%H:%M:%S.%e] [%^%l%$] [t:%t] [%s:%#] %v");
+    file_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%s:%#] %v");
 
-    m_ConsoleSink->set_pattern("[%H:%M:%S.%e] [%^%l%$] [t:%t] [%s:%#] %v");
-    m_FileSink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%s:%#] %v");
-
-    m_Sinks.emplace_back(m_ConsoleSink);
-    m_Sinks.emplace_back(m_FileSink);
+    m_Sinks.emplace_back(console_sink);
+    m_Sinks.emplace_back(file_sink);
 
     m_Logger =
         CreateRef<spdlog::logger>("rs_log", begin(m_Sinks), end(m_Sinks));
@@ -45,13 +42,13 @@ class Logger final : public Singleton<Logger> {
         spdlog::level::warn);  // Flush automatically when warning appears
 
     spdlog::register_logger(m_Logger);  // Register Logger
+    spdlog::flush_every(std::chrono::seconds(1));
     spdlog::set_default_logger(m_Logger);
+    spdlog::set_level(spdlog::level::trace);
   }
 
  private:
   Ref<spdlog::logger> m_Logger;
-  Ref<spdlog::sinks::stdout_color_sink_mt> m_ConsoleSink;
-  Ref<spdlog::sinks::basic_file_sink_mt> m_FileSink;
   std::vector<spdlog::sink_ptr> m_Sinks{};
 };  // Logger
 
