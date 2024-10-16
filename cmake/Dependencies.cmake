b72c4583813b8c52ecc74674b72ba1fc6a5dfa56
Nauris Linde <naurislinde@gmail.com>
2024-10-16 15:39:38 +0300

    feat: use cmake project for macros

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,23 +1,21 @@
-include(cmake/modules/CPM.cmake)
-
 function(setup_dependencies)
-    if(USE_SENTRY AND NOT TARGET sentry::sentry)
-      CPMAddPackage(
-        NAME sentry
-        VERSION 0.6.7
-        GITHUB_REPOSITORY getsentry/sentry-native
-        GIT_TAG "0.6.7"
-        OPTIONS "SENTRY_INTEGRATION_QT ON" "SENTRY_BUILD_TESTS OFF" "SENTRY_BUILD_EXAMPLES OFF"
-        SYSTEM YES
-      )
-    endif()
+  if(USE_SENTRY AND NOT TARGET sentry::sentry)
+    CPMAddPackage(
+      NAME sentry
+      VERSION 0.6.7
+      GITHUB_REPOSITORY getsentry/sentry-native
+      GIT_TAG "0.6.7"
+      OPTIONS "SENTRY_INTEGRATION_QT ON" "SENTRY_BUILD_TESTS OFF" "SENTRY_BUILD_EXAMPLES OFF"
+      SYSTEM YES
+    )
+  endif()
 
-    if(USE_SPDLOG AND NOT TARGET spdlog::spdlog)
-      CPMAddPackage(
-        NAME spdlog
-        VERSION 1.12.0
-        GITHUB_REPOSITORY gabime/spdlog
-        SYSTEM YES
-      )
-    endif()
+  if(USE_SPDLOG AND NOT TARGET spdlog::spdlog)
+    CPMAddPackage(
+      NAME spdlog
+      VERSION 1.12.0
+      GITHUB_REPOSITORY gabime/spdlog
+      SYSTEM YES
+    )
+  endif()
 endfunction()
